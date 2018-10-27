f6dace6cc1ef4641f5835d727b40277a0a8c4b5b
blonski <piotrblonski96@gmail.com>
2018-10-27 10:18:58 +0200

    10:18

---
+++ Cloud/CodeAndCloud.Core/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeAndCloud.Core
+{
+    class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"
+                                        Server=(localdb)\MSSQLLocalDB;
+                                        Database=CodeAndCloud;
+                                        Trusted_Connection=true;");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ Cloud/CodeAndCloud.Core/Entities/ContactModes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeAndCloud.Core.Entities
+{
+    class ContactModes
+    {
+        public virtual  string Email { get; set; }
+        public virtual string Name { get; set; }
+
+        public virtual string PhoneNumber { get; set; }
+        public virtual DateTime CreateDate { get; set; }
+        public virtual bool IsAnswer { get; set; }
+
+    }
+}
